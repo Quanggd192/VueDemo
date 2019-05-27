6ebb724fd39527a54bac002007b35b96b4f48af6
Quang <quangnd@pexdia.com>
2019-05-27 13:32:27 +0700

    a

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,43 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Todos v-bind:todos="todos" v-on:del-todo="delTodo"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Todos from "./components/todos.vue"
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld, Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo 1",
+          complete: false
+        },
+        {
+          id: 2,
+          title: "todo 2",
+          complete: false
+        },
+        {
+          id: 3,
+          title: "todo 3",
+          complete: false
+        }
+      ]
+    }
+  },
+  methods:{
+    delTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 }
 </script>
---
+++ src/components/todos.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Todo: </h1>
+        <div v-bind:key="item.id" v-for="item in todos">
+            <TodoItem v-bind:todo="item" v-on:deleteTodo="$emit('del-todo',item.id)"/>
+        </div>
+    </div>    
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: "Todos",
+    props: ["todos"],
+    components:{
+        TodoItem
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
+
